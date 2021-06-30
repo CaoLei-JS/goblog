6bb14a8728204cde34afc886988e72e5c6a2a275
曹磊 <lcaotx@gmail.com>
2021-07-01 00:22:33 +0800

    文章列表

--- main.go
+++ main.go
@@ -114,11 +114,32 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func articlesIndexHandler(w http.ResponseWriter, _ *http.Request) {
-	_, err := fmt.Fprint(w, "访问文章列表")
+func (a Article)Link() string  {
+	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(a.ID,10))
 	if err != nil {
-		return
+		checkError(err)
+		return ""
 	}
+	return showURL.String()
+}
+
+func articlesIndexHandler(w http.ResponseWriter, _ *http.Request) {
+	rows, err := db.Query("SELECT * FROM articles")
+	checkError(err)
+	defer rows.Close()
+	var articles []Article
+	for rows.Next() {
+		var article Article
+		err := rows.Scan(&article.ID, &article.Title, &article.Body)
+		checkError(err)
+		articles = append(articles, article)
+	}
+
+	err = rows.Err()
+	checkError(err)
+	tmpl, err := template.ParseFiles("resources/views/articles/index.gohtml")
+	checkError(err)
+	tmpl.Execute(w, articles)
 }
 
 // ArticlesFormData 创建博文表单数据
